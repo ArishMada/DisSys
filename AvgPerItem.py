Fixing the average per month

AvgPerItem.py
@@ -1,23 +1,14 @@
 import pandas as pd
 
-df = pd.read_csv("Sales Transactions 1.csv", encoding = "windows-1252")
+df = pd.read_csv("AverageQuantityPerMonthPerItem.csv", encoding="windows-1252")
 print(df)
 
-# Convert the 'Date' column to datetime format
-df['Date'] = pd.to_datetime(df['Date'])
-
-# Filter data for the year 2019
-df_2019 = df[df['Date'].dt.year == 2019]
-
 # Calculate the average quantity sold per item for the year 2019
-average_quantity_2019 = df_2019.groupby('ProductName')['Quantity'].mean().reset_index()
+df['MeanValue'] = df[['December_2018', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December_2019']].mean(axis=1)
 
-average_quantity_2019['Quantity'] = average_quantity_2019['Quantity'].round(3)
-average_quantity_2019 = average_quantity_2019.rename(columns={'Quantity': 'QuantityPerYear'})
+average_quantity_2019 = df[['ProductName', 'MeanValue']].copy()
 
-# Print or display the result
+
 print(average_quantity_2019)
-
-average_quantity_2019 = average_quantity_2019.fillna(0)
 
 average_quantity_2019.to_csv('AverageQuantityPerItem2019.csv', index=False)

Dataset.py
@@ -19,20 +19,20 @@ merged_df2 = pd.merge(merged_df1, additional_info2, on='ProductName', how='left'
 
 nan_count_per_column = merged_df1.isna().sum()
 
-count_of_value = merged_df1['QuantityPerYear'].value_counts().get(0, 0)
+count_of_value = merged_df1['MeanValue'].value_counts().get(0, 0)
 
 print(count_of_value)
 
 merged_df2 = merged_df2.fillna(0)
 print(merged_df1)
 
-count_negative_values = (merged_df1['QuantityPerYear'] < 0).sum()
+count_negative_values = (merged_df1['MeanValue'] < 0).sum()
 
 print("Number of negative values in column:")
 print(count_negative_values)
 
 # Create the new column with the ceiling values
-merged_df2['RestockPoint'] = np.ceil(merged_df2['QuantityPerYear'])
+merged_df2['RestockPoint'] = np.ceil(merged_df2['MeanValue'])
 
 # Create a new column Rating
 merged_df2['Rating'] = np.random.normal(loc=4.2, scale=1.0, size=len(merged_df2))