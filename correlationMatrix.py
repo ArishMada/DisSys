c51a81ac23643884c1d25ec636c684b33856f4d0
Jacques7103 <91464349+Jacques7103@users.noreply.github.com>
2023-11-20 19:37:49 +0700

    Added comments

--- correlationMatrix.py
+++ correlationMatrix.py
@@ -1,7 +1,9 @@
+#Import modules
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
 
+#Read csv
 data=pd.read_csv("inventory1.csv")
 print(data.head())
 
@@ -9,7 +11,7 @@ print(data.columns)
 
 data_scaled = data.iloc[:, 1:]
 
-#now plot correlation matrix
+#Plot correlation matrix
 correl=data_scaled.corr()
 ax=plt.subplots(figsize=(15,9))
 sns.heatmap(correl,vmax=0.8,square=True)
