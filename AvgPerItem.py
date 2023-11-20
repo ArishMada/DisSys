a3b8bcfa9b28cca0668336638dead171f6f469e5
Jacques7103 <91464349+Jacques7103@users.noreply.github.com>
2023-11-20 18:15:46 +0700

    Clean Code

--- AvgPerItem.py
+++ AvgPerItem.py
@@ -3,11 +3,9 @@ import pandas as pd
 df = pd.read_csv("AverageQuantityPerMonthPerItem.csv", encoding="windows-1252")
 print(df)
 
-# Calculate the average quantity sold per item for the year 2019
 df['MeanValue'] = df[['December_2018', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December_2019']].mean(axis=1)
 
 average_quantity_2019 = df[['ProductName', 'MeanValue']].copy()
-
 
 print(average_quantity_2019)
 
