4b6819b4512f011713b73160330a5f6fad557db4
Jacques7103 <91464349+Jacques7103@users.noreply.github.com>
2023-11-20 19:34:08 +0700

    Added comments

--- TransactionNo.py
+++ TransactionNo.py
@@ -1,13 +1,15 @@
+#Import module
 import pandas as pd
 
+#Set pandas display max 10 columns
 pd.set_option('display.max_columns', 10)
-# Assuming your DataFrame is named 'your_dataframe'
+
+#Read csv
 df = pd.read_csv('Sales Transactions 1.csv')
 
 # Group by 'ProductNo' and count the number of transactions per item
 transactions_per_item = df.groupby('ProductNo')['TransactionNo'].count().reset_index()
 
-# Print or display the result
 print(transactions_per_item)
 
 # Convert the 'Date' column to datetime format
@@ -22,7 +24,7 @@ transactions_per_month_per_item = df.groupby(['ProductName', 'YearMonth'])['Tran
 # Calculate the average number of transactions per month for each product
 average_transactions_per_month = transactions_per_month_per_item.groupby('ProductName')['TransactionNo'].mean().reset_index().round(3)
 
-# Print or display the result
 print(average_transactions_per_month)
 
+#Export result to csv
 average_transactions_per_month.to_csv('AverageTransactionPerItem.csv', index=False)
