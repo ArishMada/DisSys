f83d0f00ee41e58fcc910740baea19480ccfbd5b
Jacques7103 <91464349+Jacques7103@users.noreply.github.com>
2023-11-20 18:26:01 +0700

    Added comments

--- Dataset.py
+++ Dataset.py
@@ -1,31 +1,30 @@
+#Import modules
 import numpy as np
 import pandas as pd
 
+#Read csv
 df = pd.read_csv("inventory.csv", encoding = "windows-1252")
 print(df)
 
 additional_info = pd.read_csv('AverageQuantityPerMonthPerItem.csv')
-
 additional_info1 = pd.read_csv('AverageQuantityPerItem2019.csv')
-
 additional_info2 = pd.read_csv('AverageTransactionPerItem.csv')
 
 # Merge the two DataFrames based on the 'ProductNo' column
 merged_df = pd.merge(df, additional_info, on='ProductName', how='left')
-
 merged_df1 = pd.merge(merged_df, additional_info1, on='ProductName', how='left')
-
 merged_df2 = pd.merge(merged_df1, additional_info2, on='ProductName', how='left')
 
+#Get the number of NaN values in MeanValue
 nan_count_per_column = merged_df1.isna().sum()
-
 count_of_value = merged_df1['MeanValue'].value_counts().get(0, 0)
-
 print(count_of_value)
 
+#Fill NaN value with 0
 merged_df2 = merged_df2.fillna(0)
 print(merged_df1)
 
+#Get the number of negative values in MeanValue
 count_negative_values = (merged_df1['MeanValue'] < 0).sum()
 
 print("Number of negative values in column:")
@@ -41,17 +40,6 @@ merged_df2['Rating'] = merged_df2['Rating'].clip(0.0, 5.0)
 
 print(merged_df1)
 
-# median_value = merged_df1['Rating'].median()
-# mean_value = merged_df1['Rating'].mean()
-#
-# print(f"The median of is: {median_value}")
-# print(f"The mean of is: {mean_value}")
-#
-# count_above_3_5 = (merged_df1['Rating'] > 3.5).sum()
-#
-# print(count_above_3_5)
-
-
 bio_column = np.random.choice([0, 1], size=len(merged_df1), p=[0.80, 0.20])
 merged_df2['Bio'] = bio_column
 
@@ -59,8 +47,7 @@ count_above_1 = merged_df2.isna().sum()
 
 print("value count", count_above_1)
 
-
-
-
 print(merged_df2)
+
+#Export result to csv
 merged_df2.to_csv('inventory1.csv', index=False)
